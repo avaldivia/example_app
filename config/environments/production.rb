12caecc441c36847806093dee81b046f7c5d9bda
Juan Ruiz <invitado@perdidita.(none)>
2014-04-01 18:20:12 +0100

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,11 @@ SampleApp::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   config.serve_static_assets = true
+
+
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+    # and use secure cookies.
+  config.force_ssl = true
+
+  
 end
